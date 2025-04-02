cf87efa1a5a70b9bea7f7e9efbafc400018cd80c
dilli babu <dilli4904@gmail.com>
2025-04-02 08:37:22 +0530

    new changes

--- src/pages/E-Album/landing.tsx
+++ src/pages/E-Album/landing.tsx
@@ -1,70 +1,99 @@
 import React, { useCallback, useEffect } from "react";
 import { Card, Row, Col } from "antd";
 import { useNavigate } from "react-router-dom";
-import { getApiRouteEAlbum, getApiRouteGallarey } from "../../helpers/Common_functions";
+import { getApiRouteEAlbum } from "../../helpers/Common_functions";
 import { ApiRequest } from "../../services/api/apiService";
 import { dynamic_request, useDynamicSelector } from "../../services/redux";
 import { useDispatch } from "react-redux";
 import { Dispatch } from "redux";
-import { Album, EAlbum } from "../../routes/types/routeConfig";
+import { EAlbum } from "../../routes/types/routeConfig";
 
-const EAlbumLading: React.FC = () => {
+const EAlbumLanding: React.FC = () => {
   const navigate = useNavigate();
   const dispatch: Dispatch<any> = useDispatch();
+
   const callBackServer = useCallback(
     (variables: ApiRequest, key: string) => {
       dispatch(dynamic_request(variables, key));
     },
     [dispatch]
   );
+
   const getRoute = getApiRouteEAlbum("GetAll");
   const { loading, items } = useDynamicSelector(getRoute.identifier);
-
   const galleryItems: EAlbum[] = items?.result || [];
-  const getAllEAlbum = () => {
+
+  const getAllEAlbum = useCallback(() => {
     callBackServer(
       { method: getRoute.method, endpoint: getRoute.endpoint, data: {} },
       getRoute.identifier
     );
-  };
-  const openGallery = (albumId: number) => {
-    const selectedAlbum = galleryItems.find((album) => album._id === albumId);
-    navigate(`/ealbum/${albumId}`, { state: { pdfUrl: selectedAlbum?.albumurl } });
-  };
+  }, [callBackServer, getRoute]);
+
   useEffect(() => {
     getAllEAlbum();
-  }, []);
-  return (
-    <>
-      <div style={{ padding: 20 }}>
-        <h1 style={{ textAlign: "center", marginBottom: 20 }}>E Album</h1>
-        <Row gutter={[16, 16]}>
-          {galleryItems.map((album) => (
-            <Col xs={24} sm={12} md={8} lg={6} key={album._id}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt={album.albumname}
-                    src={album.albumcover}
-                    style={{
-                      height: "200px",
-                      objectFit: "cover",
-                      borderRadius: "8px",
-                      minHeight: "200px",
-                    }}
-                  />
-                }
-                onClick={() => openGallery(album._id)}
-              >
-                <Card.Meta title={album.albumname} />
-              </Card>
-            </Col>
-          ))}
-        </Row>
+  }, [getAllEAlbum]);
+
+  const openGallery = (albumId: number) => {
+    const selectedAlbum = galleryItems.find((album) => album._id === albumId);
+    if (selectedAlbum) {
+      navigate(`/ealbum/${albumId}`, {
+        state: { pdfUrl: selectedAlbum.albumurl },
+      });
+    }
+  };
+
+  if (loading) {
+    return <div style={centerStyle}>Loading...</div>;
+  }
+
+  if (!galleryItems.length) {
+    return (
+      <div style={centerStyle}>
+        No Albums Found
+        <br />
+        Please Contact Fresh Focuz Studio
+        <br />
+        9677894094
+        <br />
+        freshfocuzstudio@gmail.com
       </div>
-    </>
+    );
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1 style={{ textAlign: "center", marginBottom: 20 }}>E Album</h1>
+      <Row gutter={[16, 16]}>
+        {galleryItems.map((album) => (
+          <Col xs={24} sm={12} md={8} lg={6} key={album._id}>
+            <Card
+              hoverable
+              cover={
+                <img
+                  alt={album.albumname}
+                  src={album.albumcover}
+                  style={imageStyle}
+                />
+              }
+              onClick={() => openGallery(album._id)}
+            >
+              <Card.Meta title={album.albumname} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
   );
 };
 
-export default EAlbumLading;
+const centerStyle: React.CSSProperties = { textAlign: "center", marginTop: 20 };
+
+const imageStyle: React.CSSProperties = {
+  height: "200px",
+  objectFit: "cover",
+  borderRadius: "8px",
+  minHeight: "200px",
+};
+
+export default EAlbumLanding;
--- src/pages/E-Gallery/index.tsx
+++ src/pages/E-Gallery/index.tsx
@@ -1,13 +1,15 @@
 import React, { useCallback, useEffect } from "react";
 import { Card, Row, Col } from "antd";
 import { useNavigate } from "react-router-dom";
-import { getApiRouteEGallery, getApiRouteGallarey } from "../../helpers/Common_functions";
+import {
+  getApiRouteEGallery,
+  getApiRouteGallarey,
+} from "../../helpers/Common_functions";
 import { ApiRequest } from "../../services/api/apiService";
 import { dynamic_request, useDynamicSelector } from "../../services/redux";
 import { useDispatch } from "react-redux";
 import { Dispatch } from "redux";
 import { EAlbum } from "../../routes/types/routeConfig";
-
 
 const AlbumPage: React.FC = () => {
   const navigate = useNavigate();
@@ -31,7 +33,9 @@ const AlbumPage: React.FC = () => {
   const openGallery = (albumId: number) => {
     const album = galleryItems.find((item) => item._id === albumId);
     if (album) {
-      navigate(`/egallery/${albumId}`, { state: { FOLDER_ID: album?.drive_folder_id } });
+      navigate(`/egallery/${albumId}`, {
+        state: { FOLDER_ID: album?.drive_folder_id },
+      });
     }
   };
   useEffect(() => {
@@ -39,33 +43,47 @@ const AlbumPage: React.FC = () => {
   }, []);
   return (
     <>
-      <div style={{ padding: 20 }}>
-        <h1 style={{ textAlign: "center", marginBottom: 20 }}>E Gallery</h1>
-        <Row gutter={[16, 16]}>
-          {galleryItems.map((album) => (
-            <Col xs={24} sm={12} md={8} lg={6} key={album._id}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt={album.name}
-                    src={album.album_cover}
-                    style={{
-                      height: "200px",
-                      objectFit: "cover",
-                      borderRadius: "8px",
-                      minHeight: "200px",
-                    }}
-                  />
-                }
-                onClick={() => openGallery(album._id)}
-              >
-                <Card.Meta title={album.name} />
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </div>
+      {items?.result?.length > 0 ? (
+        <div style={{ padding: 20 }}>
+          <h1 style={{ textAlign: "center", marginBottom: 20 }}>E Gallery</h1>
+          <Row gutter={[16, 16]}>
+            {galleryItems.map((album) => (
+              <Col xs={24} sm={12} md={8} lg={6} key={album._id}>
+                <Card
+                  hoverable
+                  cover={
+                    <img
+                      alt={album.name}
+                      src={album.album_cover}
+                      style={{
+                        height: "200px",
+                        objectFit: "cover",
+                        borderRadius: "8px",
+                        minHeight: "200px",
+                      }}
+                    />
+                  }
+                  onClick={() => openGallery(album._id)}
+                >
+                  <Card.Meta title={album.name} />
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+      ) : (
+        <div style={{ textAlign: "center", marginTop: 20 }}>
+          No Gallery Found
+          <br />
+          Please Contact Fresh Focuz Studio
+          <br />
+          9677894094
+          <br /> freshfocuzstudio@gmail.com
+        </div>
+      )}
+      {loading && (
+        <div style={{ textAlign: "center", marginTop: 20 }}>Loading...</div>
+      )}
     </>
   );
 };
